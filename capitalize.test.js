c71e1aee3e7a394a74876b7930d7ed3899195f47
kenhardika <kenzamahardika@gmail.com>
2022-08-22 07:56:40 +0700

    add initial files, basic test

---
+++ capitalize.test.js
@@ -0,0 +1,20 @@
+// test('two plus two is four', () => {
+//     expect(2 + 2).toBe(4);
+//   });
+
+import capitalize from "./capitalize"
+
+test('Check empty input', ()=>{
+    expect(capitalize()).toBe('Error: no input')
+});
+test('Check empty input string', ()=>{
+    expect(capitalize('        ')).toBe('Error: no input')
+});
+
+test('Check number input', ()=>{
+    expect(capitalize(123)).toBe('Error: input are number')
+});
+
+test('Check number inside of string', ()=>{
+    expect(capitalize('15')).toBe('Error: input are number')
+});
