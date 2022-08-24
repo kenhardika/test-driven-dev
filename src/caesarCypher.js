aaa7058cae803cf7b4a9e0b4c403b43eeb0f9d4b
kenhardika <kenzamahardika@gmail.com>
2022-08-24 07:51:04 +0700

    Caesar Cypher done

--- src/caesarCypher.js
+++ src/caesarCypher.js
@@ -2,33 +2,51 @@
 // its an old text based cyphering technique made by Julius Caesar himself.
 // basically, you set the cypher 'shift' number then, that number will be shifting letter ex: A=0, B=1. C=2. Translate A with shift= 2. Amswer is A = C,
 // because cypherLetter = shiftNum + letterNum so 2+0 = 2, what got number 2? thats "C".
-console.log("Caesar Cypher");
 
-function cypher(word) {
+function cypher(word, shift) {
+  const LOW_POINT = 97; // 
+  const HIGH_POINT = 122;
+  const TOTAL_LETTER = 26;
   //guard clause
   if (!word) {
-    return;
+    return
+  }
+  if (!shift && shift !== 0) {
+    return 
+  }
+  if (typeof shift !== "number") {
+    return
   }
   // slice the word into each letter then put into filter, its either cypher or passthrough
   else {
     const slicedWord = word.split("");
-    // console.log(slicedWord);
     let cyphered = slicedWord.map((letter) => {
       if (/[a-zA-Z]/.test(letter)) {
         //change the letter to coded
-
-        // console.log(letter);
-        return letter.toUpperCase();
+        const alphaValConv = (val) => {
+          // low point a=97
+          // high point z=122
+          // how we are looping through from 122 back to 97
+          // possibility 1, if (val>122) then val - 26;
+          let alphaNum = val.toLowerCase().charCodeAt(0) + shift;
+          // let alphaNum = val.toLowerCase().charCodeAt(0) - 97 + shift;
+          // console.log(alphaNum);
+          if (alphaNum > HIGH_POINT) {
+            return String.fromCharCode(alphaNum - TOTAL_LETTER);
+          } else if (alphaNum < LOW_POINT) {
+            return String.fromCharCode(alphaNum + TOTAL_LETTER);
+          } else {
+            return String.fromCharCode(alphaNum);
+          }
+        };
+        return alphaValConv(letter);
       } else {
         //passthrough
-        // console.log(letter)
         return letter;
       }
     });
-    // console.log(cyphered);
     return cyphered.join("");
   }
 }
-console.log(typeof cypher("!"));
-console.log(cypher("azkan ! 12"));
+console.log(cypher("you wanna play lets PLAY! 12 times ok?", 1));
 export { cypher };
--- src/caesarCypher.test.js
+++ src/caesarCypher.test.js
@@ -6,10 +6,22 @@ test('Test Empty', ()=>{
     expect(cypher()).toBe();
 });
 test('Test Symbol Passthrough ', ()=>{
-     expect(cypher("!")).toBe("!");
-     expect(cypher("?")).toBe(`?`);
-     expect(cypher("!?")).toBe('!?');
-     expect(cypher("??>")).toBe(`??>`);
-     expect(cypher(' ')).toBe(' ');
+     expect(cypher("!", 0)).toBe("!");
+     expect(cypher("?", 0)).toBe(`?`);
+     expect(cypher("!?", 0)).toBe('!?');
+     expect(cypher("??>", 0)).toBe(`??>`);
+     expect(cypher(' ', 0)).toBe(' ');
 });
+
+test('Test Convert with shift 1', ()=>{
+    expect(cypher('a', 1)).toBe('b');
+    expect(cypher('abba', 1)).toBe('bccb');
+    expect(cypher('zccy', 1)).toBe('addz');
+})
+
+test('Test Convert with shift -1', ()=>{
+    expect(cypher('a', -1)).toBe('z');
+    expect(cypher('abba', -1)).toBe('zaaz');
+    expect(cypher('zccy', -1)).toBe('ybbx');
+})
 // !/^[a-zA-Z0-9]+$/
